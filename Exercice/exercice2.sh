62d690b6ee95a2f4a33d3242b75af731a3952abd
MathildeCh <mathildecharletb@gmail.com>
2022-10-19 10:04:02 +0200

    mon premier if statement

--- Exercice/exercice2.sh
+++ Exercice/exercice2.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/bash
 
-rm sortie-exo2.txt
-rm listes_lieux.txt
-
-if [[ -z $3 ]];
+if [ -z $3 ]
 then 
     echo "Not enough parameter passed."
     exit
@@ -13,16 +10,7 @@ ANNEE=$1
 MOIS=$2
 NB=$3
 
-
 echo "classement des $NB lieux(x) les plus cites en $MOIS-$ANNEE" > sortie-exo2.txt
-egrep  "Location"  $ANNEE\_$MOIS\_*.ann | cut -f 3 | sort | uniq -c | sort -nr >> listes_lieux.txt
-
-sed "${NB}q" listes_lieux.txt >> sortie-exo2.txt
-
-#while [[ $i -le $NB ]]
-#do
-#echo 
-#((i+=1))
-#done
+egrep  "Location"  $ANNEE\_$MOIS\_*.ann | cut -f 3 | sort | uniq -c | sort -nr | HEAD -n $NB >> sortie-exo2.txt
 
 
