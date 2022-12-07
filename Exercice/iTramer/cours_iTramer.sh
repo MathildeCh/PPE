69b77250294ac941ac148db7bc55b705bfa6f102
MathildeCh <mathildecharletb@gmail.com>
2022-12-07 09:27:59 +0100

    cours iTramer

--- Exercice/iTramer/cours_iTramer.sh
+++ Exercice/iTramer/cours_iTramer.sh
@@ -1,25 +1,28 @@
 #!/usr/bin/env bash
 
+# ATTENTION ce script doit être lancé depuis la racine du projet
 #arguments
 if [[ $# -ne 2 ]]
 then
-    echo "Deux arguments attendus"
+    echo "Deux arguments sont attendus : <dossier> <langue>"
     exit
 fi
 
-folder=$1 #dump.text
+folder=$1 #DUMPS-TEXT
 basename=$2 #la langue : fr, it, ...
 
-echo "<lang=\"$basename\">" > "itrameur/$folder-$basename.txt"
+echo "<lang=\"$basename\">" > "ITRAMEUR/$folder-$basename.txt"
 
 for filepath in $(ls $folder/$basename-*.txt)
 do
-    #filepath == dumps-texts/fr-1.txt
+    #filepath == DUMPS-TEXT/fr-1.txt
     #==> pagename = fr-1
     pagename=$(basename -s .txt $filepath)
 
-    echo "<page!\"$pagename\">" >> "itrameur/$folder-$basename.txt"
-    echo "<text>" >> "itrameur/$folder-$basename.txt"
+    echo "<page!\"$pagename\">" >> "ITRAMEUR/$folder-$basename.txt"
+    # POINT IMP : "./" n'est pas obligatoire.
+    # Mettre le chemin entre " " est une bonne habitude, plus safe : interpolation avec le nom des variables
+    echo "<text>" >> "ITRAMEUR/$folder-$basename.txt"
 
     #on récupère les dumps/contexte
     #et on écrit à l'inté de la balise text
@@ -28,9 +31,9 @@ do
     content=$(echo "$content" | sed 's/&/&amp/g')
     content=$(echo "$content" | sed 's/</&lt/g')
     content=$(echo "$content" | sed 's/</&gt/g')
-    echo "$content" >> "itrameur/$folder-$basename.txt"
+    echo "$content" >> "ITRAMEUR/$folder-$basename.txt"
 
-    echo "</text>" >> "itrameur/$folder-$basename.txt"
-    echo "</page>" >> "itrameur/$folder-$basename.txt"
+    echo "</text>" >> "ITRAMEUR/$folder-$basename.txt"
+    echo "</page>" >> "ITRAMEUR/$folder-$basename.txt"
 
 done
